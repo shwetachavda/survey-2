df6b07cfc40529d3b297cdd1dc67774fb5f89d6e
shwetachavda <shwetachavda13@gmail.com>
2016-04-08 12:33:17 -0400

    updated css

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'url': 'mongodb://survey:survey@ds028559.mlab.com:28559/survey'
+};
+
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,3 @@
+module.exports = {
+    'url':'mongodb://survey:survey@ds028559.mlab.com:28559/survey'
+};
